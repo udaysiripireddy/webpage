ac1322539e93cf545f454a042ded1ebe877107ef
udaysiripireddy <udaysiripireddy7@gmail.com>
2024-07-10 14:46:32 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,9 @@
+function handlesubmit(){
+    let form_button = document.getElementById("button_action");
+    if(form_button){
+        window.alert("User Registered")
+    }
+    else{
+    window.alert("no user")
+    }
+}
